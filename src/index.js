f8a476cffd0ff81473f897bfffda0e7ec5a486bc
chops318 <chops318@gmail.com>
2016-09-12 17:04:14 -0700

    Bad files

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
 require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
+
 const angular = require('angular');
-const cptApp = angular.module('cptApp', []);
+require('angular-ui-router');
+
+const cptApp = angular.module('cptApp', [require('angular-jwt'), 'ui.router']);
+
+cptApp.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function ($stateProvider, $locationProvider, $urlRouterProvider) {
+  //$locationProvider.html5Mode(true);
+  $locationProvider.hashPrefix('!');
+
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider.state('home', {
+    url: '/home',
+    template: require('./html/index.html'),
+  })
+  .state('/about', {
+    url: '/about',
+    template: require('./html/home.html')
+  });
+}]);
