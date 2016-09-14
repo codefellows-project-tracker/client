21c2acc1baa267faebdd0c6566226dc8ea347a3b
chops318 <chops318@gmail.com>
2016-09-14 10:53:27 -0700

    added controller

--- src/components/project-form/project-form-component.js
+++ src/components/project-form/project-form-component.js
@@ -2,9 +2,7 @@
 
 module.exports = function(app) {
   app.component('cptProjectForm', {
-    controller: function() {
-      this.lol = 'lol';
-    },
     template: require('./project-form-template.html'),
+    controller: 'ProjectController',
   });
 };
